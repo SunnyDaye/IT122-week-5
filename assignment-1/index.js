ad28012c98100875d96397f53164588d3f62c067
Suncerie Daye <sunceried@gmail.com>
2023-11-01 17:39:28 -0700

    Assignment 1 complete

---
+++ assignment-1/index.js
@@ -0,0 +1,11 @@
+async function logJSONData() {
+  const response = await fetch(
+    "https://dog-api.kinduff.com/api/facts?number=10"
+  );
+
+  const jsonData = await response.json();
+
+  console.log(jsonData);
+}
+
+logJSONData();
